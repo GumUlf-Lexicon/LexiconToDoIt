8ffe7571dd3dba6dfb2008a52122e2d18a48b8ed
GumUlf <programming@gumulf.se>
2021-10-12 21:01:06 +0200

    Created the PersonSequencer class and tests.

---
+++ LexiconToDoIt.tests/Data/PersonSequencerShould.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using LexiconToDoIt.Data;
+
+namespace LexiconToDoIt.Tests.Data
+{
+	public class PersonSequencerShould
+	{
+		[Fact]
+		public void IncreaseIdWithOneWithNextPersonId()
+		{
+			// Arrange
+			int before = PersonSequencer.PersonId;
+
+			// Act
+			int after = PersonSequencer.nextPersonId();
+
+			// Assert
+			Assert.Equal(before + 1, after);
+		}
+
+		[Fact]
+		public void SetPersonIdToZeroWithReset()
+		{
+			// Arrange
+			// Getting PersonId to be larger than 0 before reset
+			_ = PersonSequencer.nextPersonId();
+			_ = PersonSequencer.nextPersonId();
+			int before = PersonSequencer.PersonId;
+
+			// Act
+			PersonSequencer.reset();
+			int after = PersonSequencer.PersonId;
+
+			// Assert
+			// If personId already is zero, the test is meaningless
+			Assert.True(before != 0);
+			Assert.Equal(0, after);
+		}
+
+	}
+}
---
+++ LexiconToDoIt/Data/PersonSequencer.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace LexiconToDoIt.Data
+{
+	public class PersonSequencer
+	{
+		private static int personId = 0;
+
+		public static int PersonId { get => personId;}
+
+		// Get the next availible personId
+		public static int nextPersonId()
+		{
+			return personId++;
+		}
+
+		// Reset the personId to 0
+		public static void reset()
+		{
+			personId = 0;
+		}
+	}
+}
