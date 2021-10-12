fe5d9ac649491b75c746eda6cf085fbc4f319fbb
GumUlf <programming@gumulf.se>
2021-10-12 20:31:51 +0200

    Created Todo class and tests

---
+++ LexiconToDoIt.tests/Model/TodoShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+using LexiconToDoIt.Model;
+
+namespace LexiconToDoIt.Tests.Model
+{
+	public class TodoShould
+	{
+		Todo sut;
+		int todoId;
+		string decription;
+
+		// Creates shareble objects between the tests
+		public TodoShould()
+		{
+			todoId = 5;
+			decription = "Do a test!";
+			sut = new Todo(todoId, decription);
+		}
+
+		[Fact]
+		public void HaveSameIdAfterConstrution()
+		{
+			Assert.Equal(todoId, sut.TodoId);
+		}
+
+		[Fact]
+		public void HaveSameDescriptionAfterConstrution()
+		{
+			Assert.Equal(decription, sut.Description);
+		}
+
+		[Fact]
+		public void HaveDoneSetToFalseAfterContruction()
+		{
+			Assert.False(sut.Done);
+		}
+
+	}
+}
---
+++ LexiconToDoIt/Model/Todo.cs
@@ -0,0 +1,27 @@
+﻿namespace LexiconToDoIt.Model
+{
+	public class Todo
+	{
+		// Identifies the todo
+		private readonly int todoId;
+		// Descibes what to do
+		private string description;
+		// Has the todo become a isdone
+		private bool done = false;
+		// Who is should do
+		Person assignee;
+
+		public int TodoId => todoId;
+		public string Description { get => description; set => description = value; }
+		public Person Assignee { get => assignee; set => assignee = value; }
+		public bool Done { get => done; set => done = value; }
+
+		// Constructor to create a todo, that is undone.
+		public Todo(int todoId, string description)
+		{
+			this.todoId = todoId;
+			Description = description;
+		}
+
+	}
+}
