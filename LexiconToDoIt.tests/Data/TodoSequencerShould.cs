3447c5da03c8956f389bd97f586b9c858754cf1b
GumUlf <programming@gumulf.se>
2021-10-12 21:27:16 +0200

    Created TodoSequencer and tests

---
+++ LexiconToDoIt.tests/Data/TodoSequencerShould.cs
@@ -0,0 +1,55 @@
+﻿using LexiconToDoIt.Data;
+using Xunit;
+
+namespace LexiconToDoIt.Tests.Data
+{
+	public class TodoSequencerShould
+	{
+		[Fact]
+		public void IncreaseIdWithOneWithNextTodoId()
+		{
+			// Arrange
+			int before = TodoSequencer.TodoId;
+
+			// Act
+			_ = TodoSequencer.nextTodoId();
+			int after = TodoSequencer.TodoId;
+
+			// Assert
+			Assert.Equal(before + 1, after);
+		}
+
+		[Fact]
+		public void TheCurrentNextTodoIdWithNextTodoId()
+		{
+			// Arrange
+			int current = TodoSequencer.TodoId;
+
+			// Act
+			int returnedTodoId = TodoSequencer.nextTodoId();
+
+			// Assert
+			Assert.Equal(current, returnedTodoId);
+
+		}
+
+		[Fact]
+		public void SetTodoIdToZeroWithZeroOut()
+		{
+			// Arrange
+			// Getting TodoId to be larger than 0 before reset
+			_ = TodoSequencer.nextTodoId();
+			_ = TodoSequencer.nextTodoId();
+			int before = TodoSequencer.TodoId;
+
+			// Act
+			TodoSequencer.zeroOut();
+			int after = TodoSequencer.TodoId;
+
+			// Assert
+			// If TodoId already is zero, the test is meaningless
+			Assert.True(before != 0);
+			Assert.Equal(0, after);
+		}
+	}
+}
---
+++ LexiconToDoIt/Data/TodoSequencer.cs
@@ -0,0 +1,23 @@
+﻿namespace LexiconToDoIt.Data
+{
+	public class TodoSequencer
+	{
+		private static int todoId = 0;
+
+		public static int TodoId { get => todoId; }
+
+		// Get the next availible todoId
+		public static int nextTodoId()
+		{
+			return todoId++;
+		}
+
+		// Reset the todoId to 0
+		public static void zeroOut() // You said to use different method names. :-D
+		{
+			todoId = 0;
+		}
+
+
+	}
+}
