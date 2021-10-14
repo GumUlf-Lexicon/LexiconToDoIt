68163da8b0b1f33ea451e852158809b63ed2f8ae
GumUlf <programming@gumulf.se>
2021-10-14 09:55:06 +0200

    Created people class and tests

---
+++ LexiconToDoIt.tests/Data/PeopleShould.cs
@@ -0,0 +1,133 @@
+﻿using LexiconToDoIt.Data;
+using LexiconToDoIt.Model;
+using System;
+using Xunit;
+
+namespace LexiconToDoIt.Tests.Data
+{
+	public class PeopleShould
+	{
+
+
+		private void ArrangePeople(out Person[] persons, out People people)
+		{
+			// Arrange
+
+			// personId is set to -1 bc we don't know the real ID yet
+			// The persons in persons will be replaced with new persons
+			// with the correct ID set in the for-loop below.
+			persons = new Person[4];
+			persons[0] = new Person("Jane", "Doe", -1);
+			persons[1] = new Person("Joe", "Doe", -1);
+			persons[2] = new Person("Svea", "Svensson", -1);
+			persons[3] = new Person("Sven", "Svensson", -1);
+
+			people = new People();
+			people.Clear();
+
+			for(int i = 0; i < persons.Length; i++)
+			{
+				Person person = persons[i];
+				persons[i] = people.NewPerson(person.FirstName, person.LastName);
+			}
+		}
+
+		[Fact]
+		public void HaveTheRightSize()
+		{
+			// Arrange
+			ArrangePeople(out Person[] persons, out People people);
+
+			// Act
+			int peopleSize = people.Size();
+
+			// Assert
+			Assert.Equal(persons.Length, peopleSize);
+		}
+
+		[Fact]
+		public void BeAbleToFindAllPersons()
+		{
+			// Arrange
+			ArrangePeople(out Person[] persons, out People people);
+
+			// Act
+			Person[] sut = people.FindAll();
+
+			// Assert
+			Assert.Equal(persons.Length, sut.Length);
+
+			foreach(Person person in persons)
+			{
+				Assert.Contains(sut, sutPerson => sutPerson == person);
+			}
+		}
+
+		[Fact]
+		public void BeAbleToFindPersonById()
+		{
+			// Arrange
+			ArrangePeople(out Person[] persons, out People people);
+
+			foreach(Person person in persons)
+			{
+				// Act
+				Person sut = people.FindById(person.PersonId);
+
+				// Assert
+				Assert.Equal(person, sut);
+			}
+		}
+
+		[Fact]
+		public void BeAbleToCreateNewPerson()
+		{
+			// Arrange
+			string firstName = "Kim";
+			string lastName = "Karlsson";
+			People people = new People();
+			
+			// Act
+			Person sut = people.NewPerson(firstName, lastName);
+
+			// Assert
+			Assert.Equal(sut.FirstName, firstName);
+			Assert.Equal(sut.LastName, lastName);
+		}
+
+		[Theory]
+		[InlineData(null, null)]
+		[InlineData(null, "Doe")]
+		[InlineData("Jane", null)]
+		[InlineData(null, "")]
+		[InlineData("", null)]
+		[InlineData("", "")]
+		[InlineData("", "Doe")]
+		[InlineData("Joe", "")]
+		public void NotAllowANewPersonWithNullOrEmptyNames(string firstName, string lastName)
+		{
+			// Arrange
+			People people = new People();
+
+			// Act and Assert
+			_ = Assert.Throws<ArgumentException>(() => people.NewPerson(firstName, lastName));
+
+		}
+
+		[Fact]
+		public void BeAbleToGetCleared()
+		{
+			// Arrange
+			ArrangePeople(out _, out People people);
+			int peopleSizeBefore = people.Size();
+
+			// Act
+			people.Clear();
+
+			// Assert
+			Assert.True(peopleSizeBefore > 0);
+			Assert.Equal(0, people.Size());
+		}
+
+	}
+}
---
+++ LexiconToDoIt/Data/People.cs
@@ -0,0 +1,70 @@
+﻿using LexiconToDoIt.Model;
+using System;
+
+namespace LexiconToDoIt.Data
+{
+	public class People
+	{
+		private static Person[] persons = new Person[0];
+
+		public int Size()
+		{
+			return persons.Length;
+		}
+
+		public Person[] FindAll()
+		{
+			// Copy array to not return a referense to the original.
+			Person[] returnPersonArray = new Person[persons.Length];
+			Array.Copy(persons, returnPersonArray, persons.Length);
+
+			return returnPersonArray;
+		}
+
+		// Finds and returns person by ID. If not found returns null.
+		public Person FindById(int personId)
+		{
+			return Array.Find(persons, p => p.PersonId == personId);
+		}
+
+		// Creates a new person and inserts into the People database
+		// Throws argument exception if firstName or/and lastName
+		// is null or empty.
+		public Person NewPerson(string firstName, string lastName)
+		{
+			// Names are checked so that they are correct and new person can be created
+			if(string.IsNullOrEmpty(firstName) && string.IsNullOrEmpty(lastName))
+			{
+				throw new ArgumentException("Neither fistName or lastName may be null or empty!");
+			}
+			else if(string.IsNullOrEmpty(firstName))
+			{
+				throw new ArgumentException("firstName may not be null or empty!");
+			}
+			else if(string.IsNullOrEmpty(lastName))
+			{
+				throw new ArgumentException("lastName may not be null or empty!");
+			}
+
+			// A new unique personId is needed to create the new person
+			int personId = PersonSequencer.nextPersonId();
+
+			Person newPerson = new Person(firstName, lastName, personId);
+
+			// Resize the array so that there is room for a new person
+			Array.Resize(ref persons, persons.Length + 1);
+
+			// To get the database into a valid state with the new person
+			persons[^1] = newPerson;
+			
+			return newPerson;
+		}
+
+		// Empties the People database
+		public void Clear()
+		{
+			persons = Array.Empty<Person>();
+		}
+
+	}
+}
